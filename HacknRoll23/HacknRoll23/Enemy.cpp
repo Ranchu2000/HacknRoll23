92ffce803f33479250f488a544a187587997ae41
sktlim <suikiat00@gmail.com>
2023-01-14 14:07:43 +0800

    Added initial enemy files

---
+++ HacknRoll23/HacknRoll23/Enemy.cpp
@@ -0,0 +1,58 @@
+#include "Enemy.h"
+
+void Enemy::initVariables() {
+
+}
+
+void Enemy::initAnimations() {
+
+}
+
+Enemy::Enemy(Player& player) {
+	this->initVariables();
+	this->initAnimations();
+}
+
+Enemy::~Enemy() {
+
+}
+
+const bool Enemy::getDamageTimerDone() const{
+	return this->damageTimer.getElapsedTime().asMilliseconds() >= this->damageTimerMax;
+}
+
+const bool Enemy::getDespawnTimerDone() const {
+	return this->despawnTimer.getElapsedTime().asMilliseconds() >= this->despawnTimerMax;
+}
+
+void Enemy::resetDamageTimer() {
+	this->damageTimer.restart();
+}
+
+void Enemy::resetBurnTimer() {
+	this->burnTimer.restart();
+}
+
+void Enemy::setTotalBurnDamage(int burnDamage) {
+	this->totalBurnDamage = burnDamage;
+}
+
+void Enemy::loseHP(const int hp, Vector2f projectileDirection, const float& dt) {
+	// to be implemented once entity class is up
+}
+
+const bool Enemy::isDead() const {
+	// to be implemented once entity class is up
+}
+
+const bool Enemy::isBurning() const {
+	// to be implemented once entity class is up
+}
+
+const AttributeComponent* Enemy::getAttributeComp() const {
+	// to be implemented once entity class is up
+};
+
+void Enemy::updateBurn() {
+	// to be implemented once entity class is up
+}
