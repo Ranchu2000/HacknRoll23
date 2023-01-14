816ddb54dc865452e61ae1444643310381fc64f9
Wong Yu Fei <ywong045@e.ntu.edu.sg>
2023-01-15 07:42:05 +0800

    player component

--- HacknRoll23/HacknRoll23/GameIntro.cpp
+++ HacknRoll23/HacknRoll23/GameIntro.cpp
@@ -38,19 +38,38 @@ void GameIntro::initGui()
 	this->gamePlayText.setPosition(gui::p2pX(50.0f, vm) - this->title.getGlobalBounds().width / 2, gui::p2pY(5.f, vm));
 
 	this->enemyText.setFont(this->font);
-	this->enemyText.setCharacterSize(gui::calcCharSize(vm, 30));
+	this->enemyText.setCharacterSize(gui::calcCharSize(vm, 50));
 	this->enemyText.setFillColor(Color(RED_COLOR));
 	this->enemyText.setStyle(Text::Regular);
 	this->enemyText.setString("There are 2 types of enemies that Timmy will encounter, Melee and Ranged Enemies.");
 	this->enemyText.setPosition(gui::p2pX(60.0f, vm) - this->title.getGlobalBounds().width / 2, gui::p2pY(5.f, vm));
 
+	this->enemyMeleeTexture.loadFromFile("MELEE_ENEMY_FILEPATH");
+	this->enemyMelee.setTexture(enemyMeleeTexture);
+	this->enemyMelee.setPosition(0.f, 10.f);
+
+	this->enemyRangedTexture.loadFromFile("RANGED_ENEMY_FILEPATH");
+	this->enemyRanged.setTexture(enemyRangedTexture);	
+	this->enemyRanged.setPosition(0.f, 20.f);
+
+	this->enemyProjTexture.loadFromFile("ENEMY_PROJ_FILEPATH");
+	this->enemyProj.setTexture(enemyProjTexture);
+	this->enemyProj.setPosition(0.f, 30.f);
+	
 	this->playerText.setFont(this->font);
-	this->playerText.setCharacterSize(gui::calcCharSize(vm, 30));
+	this->playerText.setCharacterSize(gui::calcCharSize(vm, 50));
 	this->playerText.setFillColor(Color(RED_COLOR));
 	this->playerText.setStyle(Text::Regular);
 	this->playerText.setString("Meet Timmy :) In the face of seemingly impossible firms, Timmy is armed with many copies of his resume which will be his ammunition. Both Timmy's player and resume stats can be upgraded during the wave intermission rounds");
 	this->playerText.setPosition(gui::p2pX(80.0f, vm) - this->title.getGlobalBounds().width / 2, gui::p2pY(5.f, vm));
 
+	this->playerTexture.loadFromFile("PLAYER_FILEPATH");
+	this->player.setTexture(playerTexture);
+	this->player.setPosition(0.f, 40.f);
+
+	this->playerProjTexture.loadFromFile("PLAYER_PROJ_FILEPATH");
+	this->playerProj.setTexture(playerProjTexture);
+	this->playerProj.setPosition(0.f, 50.f);
 
 	this->nextBtn= new gui::Button(
 		gui::p2pX(75.f, vm), gui::p2pY(87.f, vm),
@@ -103,5 +122,16 @@ void GameIntro::render(sf::RenderTarget* target)
 		target = this->window;
 	target->draw(this->background);
 	target->draw(this->title);
+	target->draw(gamePlayText);
+	target->draw(enemyText);
+	target->draw(gamePlayText);
+	target->draw(gamePlayText);
+	target->draw(playerText);
+	target->draw(enemyMelee);
+	target->draw(enemyRanged);
+	target->draw(enemyProj);
+	target->draw(player);
+	target->draw(playplayerProjer);
+	
 	this->nextBtn->render(*target);
 }
