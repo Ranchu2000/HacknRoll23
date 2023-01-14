17b7a72c81250cd1b2ca9a7f73dd1bbc4249f90b
changdaozheng <daozhengchang@gmail.com>
2023-01-14 16:00:06 +0800

    Game variables

---
+++ HacknRoll23/HacknRoll23/DEFINITIONS.h
@@ -0,0 +1,21 @@
+#ifndef header_H
+#define header_H
+
+//GAME SETTINGS
+#define GRIDSIZE 50
+#define MAPSIZE 56
+#define SCREEN_WIDTH 1920
+#define SCREEN_HEIGHT 1080
+#define TITLE "Jobpocalypse"
+#define FRAME_LIMIT 120
+
+//PLAYER
+#define PLAYER_INITIAL_SPEED 570.f
+#define PLAYER_UPGRADE_SPEED 180.f
+#define PLAYER_INITIAL_DAMAGE 2
+#define PLAYER_UPGRADE_DAMAGE 1
+#define PLAYER_INITIAL_HEALTH 20
+#define PLAYER_UPGRADE_HEALTH 5
+
+
+#endif
