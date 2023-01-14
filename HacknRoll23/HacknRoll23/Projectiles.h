ef72ffe6d9be03683c1fa54d52702d9c799afcbe
changdaozheng <daozhengchang@gmail.com>
2023-01-15 00:03:19 +0800

    Initial commit for projectiles base class

---
+++ HacknRoll23/HacknRoll23/Projectiles.h
@@ -0,0 +1,41 @@
+#ifndef projectiles_H
+#define projectiles_H
+
+#include "imports.h"
+
+class Projectiles
+{
+
+protected:
+	sf::Sprite sprite;
+	sf::Vector2f start;
+	sf::Vector2f normDir;
+	float speed;
+	sf::Vector2f velocity;
+	int damage;
+	int health;
+	bool piercingEntity; //Check if piercing arrow is still in enemy to avoid dealing extra damage
+
+
+
+public:
+	Projectiles(sf::Vector2f start, sf::Vector2f normDir, int damage, int health, sf::Texture& tex);
+	~Projectiles();
+
+	sf::Sprite getSprite();
+	const sf::Vector2f getNormDir();
+	const float getSpeed();
+	const sf::Vector2f getVelocity();
+	const int getDamage();
+	const bool isInEntity();
+
+	int reduceHealth();
+	void setPiercingEntity(bool status);
+
+	virtual void move(const float& dt);
+	void render(sf::RenderTarget& target);
+
+};
+
+#endif
+
