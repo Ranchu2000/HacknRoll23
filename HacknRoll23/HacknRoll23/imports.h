97b4cdc914df2410dd08e1105352a6e57ca165a5
Wong Yu Fei <ywong045@e.ntu.edu.sg>
2023-01-14 13:33:38 +0800

    common imports

---
+++ HacknRoll23/HacknRoll23/imports.h
@@ -0,0 +1,36 @@
+#ifndef imports_H
+#define imports_H
+
+//Core
+#include <algorithm>
+#include <deque>
+#include <iostream>
+#include <map>
+#include <memory>
+#include <set>
+#include <thread>
+#include <utility>
+#include <vector>
+#include <list>
+#include <math.h>
+#include <stack>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <cmath>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <cstdlib>
+
+//SFML
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
+
+//Custom
+using namespace sf;
+
+#endif
